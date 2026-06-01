7f00904f01d1343abd6f7d203e69634092bc4cbd
Ribeiro <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-06-01 09:15:13 -0300

    feat - Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+include("infra/db/connect.php");
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    $usuario = $_POST["usuario"];
+    $senha = $_POST["senha"];
+
+    $sql = "SELECT * FROM users 
+    WHERE username = '$usuario' 
+    AND password = '$senha'";
+
+    $resultado = $conn -> query($sql);
+
+    if($resultado -> num_rows > 0){
+        $_SESSION["usuario"] = $usuario;
+        header("Location: public/home.php");
+        exit();
+    }else{
+        $erro = "Usuário ou senha inválidos.";
+    }
+}
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login com PHP</title>
+</head>
+<body>
+     <?php
+    include("../public/component/table.php");
+    ?>
+    <h2>Login com PHP</h2><form method="POST">
+        <label for="usuario">Usuario:</label>
+        <input type="text" name="usuario">
+        <br>
+        <br>
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha">
+        <br>
+        <br>
+        <?php
+
+            if(isset($erro)){
+                echo $erro;
+            }
+        ?>
+        <button type="submit">Entrar</button>
+    </form>
+    
+
+
+    
+</body>
+</html>
