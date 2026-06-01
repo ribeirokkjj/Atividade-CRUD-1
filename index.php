3eaea12de01451fc2bcc8e6e02efa6d93fac3380
Ribeiro <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-06-01 10:15:32 -0300

    feat - adicionado comentario para o index.php

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
 
-include("infra/db/connect.php");
+include("infra/db/connect.php"); //puxa o código do connect.php
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){ //se o form com metodo post for submitado executa esse codigo
 
-    $usuario = $_POST["usuario"];
+    $usuario = $_POST["usuario"]; //coloca usuario e senha dos inputs nas variaveis
     $senha = $_POST["senha"];
 
-    $sql = "SELECT * FROM users 
+    //procura usuarios no db que estejam com os valores escritos nos inputs
+    $sql = "SELECT * FROM users
     WHERE username = '$usuario' 
     AND password = '$senha'";
 
-    $resultado = $conn -> query($sql);
+    $resultado = $conn -> query($sql); //conecta o banco de dados ao codigo
 
-    if($resultado -> num_rows > 0){
+    if($resultado -> num_rows > 0){ //o if verifica se há algum valor encontrado, se sim signfica que o login foi encontrado
         $_SESSION["usuario"] = $usuario;
         header("Location: public/home.php");
-        exit();
+        exit(); //entra no home.php e sai do index.php
     }else{
-        $erro = "Usuário ou senha inválidos.";
+        $erro = "Usuário ou senha inválidos."; //guarda o que sera escrito no aviso de erro
     }
 }
 
@@ -32,9 +33,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
      <?php
-    include("../public/component/table.php");
+    include("public/component/table.php"); //puxa a tabela de funcionários do banco de dados e mostra graficamente
     ?>
-    <h2>Login com PHP</h2><form method="POST">
+    <h2>Login com PHP</h2><form method="POST"> <!--login do html-->
         <label for="usuario">Usuario:</label>
         <input type="text" name="usuario">
         <br>
@@ -46,7 +47,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <?php
 
             if(isset($erro)){
-                echo $erro;
+                echo $erro;//mostra o erro no html caso o if entre no else lá em cima no php
             }
         ?>
         <button type="submit">Entrar</button>
